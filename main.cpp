f254e1d7e7359d4b24a2bc6673804799631ef2fc
thrawn235 <thrawn235@gmail.com>
2017-08-31 20:08:30 +0200

    reworked

--- main.cpp
+++ main.cpp
@@ -13,135 +13,215 @@ void InitCurses()
 	keypad(stdscr, true);
 	noecho();
 	start_color();
-	init_pair(1,COLOR_BLUE, COLOR_RED);
+	init_pair(1,COLOR_WHITE, COLOR_BLUE);
+	init_pair(2,COLOR_WHITE, COLOR_CYAN);//Words
+	init_pair(3,COLOR_WHITE, COLOR_MAGENTA);//Spaces
+	init_pair(4,COLOR_WHITE, COLOR_RED);//Tabs
+	init_pair(5,COLOR_WHITE, COLOR_GREEN);//special Characters
+	init_pair(6,COLOR_WHITE, COLOR_WHITE);//Endline
 	refresh();
 }
 
-class cTextBuffer
+struct vec2D
 {
-	protected:
-	vector<string> text;
-	
-	public:
-	cTextBuffer()
-	{
-	}
-	void LoadFromFile(string path)
-	{
-		ifstream file;
-		file.open(path);
-		
-		string line;
-		
-		file.seekg(0, ios_base::beg);
-		while(!file.eof())
-		{
-			getline(file, line);
-			text.push_back(line);
-		}
-			
-
-		file.close();
-	}
-	string GetLine(unsigned int lineIndex)
-	{
-		if(lineIndex < text.size())
-		{
-			return text[lineIndex];
-		}
-		return "";
-	}
-	int getNumLines()
-	{
-		return text.size();
-	}
-	//int getLength();
+	int x;
+	int y;
 };
 
 class cTextWindow
 {
-	protected:
-	int posX;
-	int posY;
+	public: //change to protected when done debugging!
+	vector<string> textBuffer;
 	WINDOW* win;
 	WINDOW* border;
-	int width;
-	int height;
-	int originY;
-	int originX;
-	cTextBuffer* buffer;
+	vec2D pos;
+	vec2D cursorPos;
+	vec2D windowPos;
+	unsigned int width; //excluding Border
+	unsigned int height; //excluding Border
+	vec2D origin;
+	bool selectMode;
+	vec2D selectStart;
+	string filePath;
+	int tabSpaces;
 	
 	public:
+	cTextWindow()
+	{
+		pos.y = 5;
+		pos.x = 10;
+		width = 60;
+		height = 15;
+		win = newwin(height, width, pos.y, pos.x);
+		border = newwin(height+2, width+2, pos.y-1, pos.x-1);
+		origin.y =0;
+		origin.x =0;
+		selectMode = false;
+		selectStart.y = 0;
+		selectStart.x = 0;
+		tabSpaces = 4;
+	}
 	cTextWindow(int newPosY, int newPosX, int newWidth, int newHeight)
 	{
-		buffer = new cTextBuffer;
-		posX = newPosX;
-		posY = newPosY;
+		pos.y = newPosY;
+		pos.x = newPosX;
 		width = newWidth;
 		height = newHeight;
-		win = newwin(height, width, posY, posX);
-		border = newwin(height+2, width+2, posY-1, posX-1);
-		originY = 0;
-		originX = 0;
-		wbkgd(win, COLOR_PAIR(1));
-		wbkgd(border, COLOR_PAIR(1));
-		box(border, 0,0);
+		win = newwin(height, width, pos.y, pos.x);
+		border = newwin(height+2, width+2, pos.y-1, pos.x-1);
+		origin.y =0;
+		origin.x =0;
+		selectMode = false;
+		selectStart.y = 0;
+		selectStart.x = 0;
+		tabSpaces = 4;
 	}
+	void LoadFromFile(string path) //loads all lines from file and strips \n (no error checking)
+	{
+		ifstream file;
+		file.open(path, ios::in);
+		if(file)
+		{
+			textBuffer.clear();
+			path = filePath;
+		
+			string line;
+			
+			file.seekg(0, ios_base::beg);
+			while(!file.eof())
+			{
+				getline(file, line);
+				
+				//replace Tabs with spaces--------
+				for(unsigned int i = 0; i < line.length(); i++)
+				{
+					if(line[i] == 9) //ascii 9 = tab
+					{
+						line[i] = ' ';
+						line.insert(i, tabSpaces-1, ' ');
+					}
+				}
+				//-------------------------------
+				
+				textBuffer.push_back(line);
+			}
+				
+
+			file.close();
+		}
+		else
+		{
+			textBuffer.push_back("this file does not exits");
+		}
+	}
+	void SaveToFile(string path) //Save File, Add \n, replace Spaces with Tabs
+	{
+		ofstream file;
+		file.open(path);
+		
+		string line;
+		for(unsigned int i = 0; i < textBuffer.size(); i++)
+		{
+			line = textBuffer[i];
+			
+			int spaceCount = 0;
+			for(unsigned int u = 0; u < line.length(); u++)
+			{
+				if(line[u] == ' ')
+				{
+					spaceCount++;
+				}
+				if(spaceCount == tabSpaces)
+				{
+					spaceCount = 0;
+					line[u] = 9;
+					line.erase(u+1, tabSpaces-1);
+				}
+			}
+			file.write(line.c_str(), line.length());
+		}
+		
+		file.close();
+	}
+	void SaveToFile()
+	{
+		ofstream file;
+		file.open(filePath);
+		
+		string line;
+		for(unsigned int i = 0; i < textBuffer.size(); i++)
+		{
+			line = textBuffer[i];
+			
+			int spaceCount = 0;
+			for(unsigned int u = 0; u < line.length(); u++)
+			{
+				if(line[u] == ' ')
+				{
+					spaceCount++;
+				}
+				if(spaceCount == tabSpaces)
+				{
+					spaceCount = 0;
+					line[u] = 9;
+					line.erase(u+1, tabSpaces-1);
+				}
+			}
+			file.write(line.c_str(), line.length());
+		}
+		
+		file.close();
+	}
+	//void MoveCursorLeft();
+	//void MoveCursorRight()
+	//void MoveCursorUp();
+	//void MoveCursorDown();
+	//void MoveCursorWordLeft();
+	//void MoveCursorWordRight();
+	//void CarriageReturn();
+	//void EnableSelect();
+	//void DisableSelect();
+	//void MoveSelectStartLeft();
+	//void MoveSelectStartRight();
+	//void MoveSelectStartWordLeft();
+	//void MoveSelectStartWordRight();
 	void Show()
 	{
-		wmove(win, 0,0);
-		for(int i = originY; i< getmaxy(win)+originY; i++) //or EOF
-		{
-			wprintw(win,"%i: %s\n", i, buffer->GetLine(i).c_str());
-		}
-		wmove(win, 0,0);
+		wbkgd(border, COLOR_PAIR(1));
+		wbkgd(win, COLOR_PAIR(1));
+		box(border, 0,0);
 		
+		wmove(win, 0,0);
+		string line;
+		for(unsigned int i = origin.y; i < (unsigned int)getmaxy(win)+origin.y && i < textBuffer.size(); i++) //or EOF
+		{
+			line = textBuffer[i];
+			wprintw(win,"%i: %s\n", i, line.c_str());
+		}
+		
+		wmove(win, cursorPos.y, cursorPos.x);
 		wrefresh(border);
-		//wrefresh(win);
-	}
-	int GetOriginY()
-	{
-		return originY;
-	}
-	void SetOriginY(int newOriginY)
-	{
-		originY = newOriginY;
-	}
-	void LoadFromFile(string path)
-	{
-		buffer->LoadFromFile(path);
-	}
-	WINDOW* GetWin()
-	{
-		return win;
-	}
-	int GetHeight()
-	{
-		return height;
+		wrefresh(win);
 	}
 };
-
 
 int main()
 {
 	
 	InitCurses();
 	
-	cTextWindow textWindow(5,5, 40, 10);
-	textWindow.LoadFromFile("main.cpp");
+	cTextWindow textWindow;
+	textWindow.LoadFromFile("./main.cpp");
 	
 	int ch = 0;
-	int cursorPosX = 0;
-	int cursorPosY = 0;
 	while(ch != 27)
 	{
+		textWindow.Show();
+		
 		ch = getch();
+			
 		
-		
-		
-		
-		
-		if(ch == KEY_UP)
+/*		if(ch == KEY_UP)
 		{
 			if(cursorPosY <= 0)
 			{
@@ -174,20 +254,12 @@ int main()
 		textWindow.Show();
 		
 		wmove(textWindow.GetWin(), cursorPosY, cursorPosX);
-		wrefresh(textWindow.GetWin());
+		wrefresh(textWindow.GetWin());*/
 		
 
 	}	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	getch();
+	//getch();
 	endwin();
 	return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <ncurses.h>
+using namespace std;
